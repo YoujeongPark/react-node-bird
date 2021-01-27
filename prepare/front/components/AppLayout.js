09e503697bb4cf473d565d555db94c1f6d2085f6
YoujeongPark <pyj0695@gmail.com>
2021-01-27 21:49:26 +0900

    proptypes & eslint

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -2,5 +2,27 @@
 // Components => components 
 
 import React from 'react';
+import PropTypes from "prop-types";
+import Link from 'next/link';
 
-const Signup = 
+const AppLayout = ({children}) => {
+    return(
+        <div>
+            <div>
+                <div> 공통메뉴 AppLayout </div>
+                <Link href = "/"><a> Node Bird </a></Link>
+                <Link href = "/profile"><a> 프로필 </a></Link>
+                <Link href = "/signup "><a> 회원가입 </a></Link>
+            </div>
+            {children}
+        </div>
+    )
+} ;
+
+
+AppLayout.proptypes = { 
+    children : PropTypes.node.isRequired,
+}; 
+
+
+export default AppLayout; 
